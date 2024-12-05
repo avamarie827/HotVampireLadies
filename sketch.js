90082dae266ceedab12bad7d37ac05d28a954610
avamarie827 <avamarie@vt.edu>
2024-12-05 13:12:05 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+//Variables//
+let bckgrd;
+
+function preload() {
+
+//Msc Images//
+bckgrd = loadImage("/Msc_assets/background.PNG");
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  bckgrd();
+
 }
