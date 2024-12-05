a132a113068d0ccd53581ae3dbf97d5e69e7c230
avamarie827 <avamarie@vt.edu>
2024-12-05 13:59:30 -0500

    a

--- sketch.js
+++ sketch.js
@@ -1,29 +1,10 @@
-//Variables//
 let bckgrd;
-<<<<<<< HEAD
 let base;
-=======
-
->>>>>>> c61308ecff52ec17922ccbd8eff07fc7d00c7903
 function preload() {
-
-//bases
-base1 = loadImage("Msc_Assets/base/base1.PNG")
-base2 = loadImage("Msc_Assets/base/base2.PNG")
-base3 = loadImage("Msc_Assets/base/base3.PNG")
-base4 = loadImage("Msc_Assets/base/base4.PNG")
-base5 = loadImage("Msc_Assets/base/base5.PNG")
-base6 = loadImage("Msc_Assets/base/base6.PNG")
-
 //Msc Images//
-<<<<<<< HEAD
 bckgrd = loadImage("Msc_assets/background.PNG");
-base = loadImage()
 
 
-=======
-bckgrd = loadImage("/Msc_assets/background.PNG");
->>>>>>> c61308ecff52ec17922ccbd8eff07fc7d00c7903
 }
 
 function setup() {
@@ -33,9 +14,5 @@ function setup() {
 function draw() {
   background(220)
   image (bckgrd, 0,0, width, height, 0,0, bckgrd.width, bckgrd.height,CONTAIN);
-<<<<<<< HEAD
   
-=======
-
->>>>>>> c61308ecff52ec17922ccbd8eff07fc7d00c7903
 }
