e9dec74ef9a7c5cd63cd0cb2a24e507f9a569086
emilyph24 <emilyph@vt.edu>
2024-12-05 14:05:36 -0500

    bases

--- sketch.js
+++ sketch.js
@@ -4,6 +4,14 @@ function preload() {
 //Msc Images//
 bckgrd = loadImage("Msc_assets/background.PNG");
 
+//bases
+
+base1 = loadImages("base/base1.PNG")
+base2 = loadImages("base/base2.PNG")
+base3 = loadImages("base/base3.PNG")
+base4 = loadImages("base/base4.PNG")
+base5 = loadImages("base/base5.PNG")
+base6 = loadImages("base/base6.PNG")
 
 }
 //test
