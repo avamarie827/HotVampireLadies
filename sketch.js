d05c3bfd94e011eca0f762b75c34222bf21b0bb0
emilyph24 <emilyph@vt.edu>
2024-12-05 13:33:06 -0500

    somethings gonna happen?

--- sketch.js
+++ sketch.js
@@ -1,10 +1,21 @@
+//Variables//
 let bckgrd;
 function preload() {
 //Msc Images//
 bckgrd = loadImage("Msc_assets/background.PNG");
 
+function preload() {
 
+//bases
+base1 = loadImage("Msc_Assets/base/base1.PNG")
+base2 = loadImage("Msc_Assets/base/base2.PNG")
+base3 = loadImage("Msc_Assets/base/base3.PNG")
+base4 = loadImage("Msc_Assets/base/base4.PNG")
+base5 = loadImage("Msc_Assets/base/base5.PNG")
+base6 = loadImage("Msc_Assets/base/base6.PNG")
 
+//Msc Images//
+bckgrd = loadImage("/Msc_assets/background.PNG");
 }
 
 function setup() {
